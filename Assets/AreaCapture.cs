8368d8d44001c4f1ff641781e80b0311ff28e1f6
RichieDevEire <c15707085@mydit.ie>
2017-03-06 00:24:10 +0000

    Trying to merge

---
+++ Assets/AreaCapture.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaCapture : MonoBehaviour 
+{
+	CaptureController capControl; 
+	// Use this for initialization
+	void Start () 
+	{
+		capControl = GameObject.Find("CaptureZone").GetComponent<CaptureController>();
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag == "GoldTeam")
+		{
+			capControl.goldTeam = true;
+		}
+		if(other.gameObject.tag == "SilverTeam")
+		{
+			capControl.silverTeam = true;
+		}
+	}
+	void OnTriggerExit(Collider other)
+	{
+		if(other.gameObject.tag == "GoldTeam")
+		{
+			capControl.goldTeam = false;
+		}
+		if(other.gameObject.tag == "SilverTeam")
+		{
+			capControl.silverTeam = false;
+		}
+	}
+}
---
+++ Assets/CaptureController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaptureController : MonoBehaviour 
+{
+	public bool goldTeam = false;
+	public bool silverTeam = false;
+
+	private float goldCapPerc = 0;
+	private float silverCapPerc = 0;
+	public float capSpeed = 2.0f;
+
+	//AREA SHOWN
+	public GameObject gold;
+	public GameObject silver;
+	public GameObject Neutral;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		gold.SetActive(false);
+		silver.SetActive(false);
+		Neutral.SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(goldTeam == true)
+		{
+			goldCapPerc += Time.deltaTime * capSpeed;
+			silverCapPerc -= Time.deltaTime * capSpeed;
+		}
+		if(silverTeam == true)
+		{
+			goldCapPerc -= Time.deltaTime * capSpeed;
+			silverCapPerc += Time.deltaTime * capSpeed;
+		}
+		if(goldTeam == true && silverTeam == true)
+		{
+			goldCapPerc = goldCapPerc;
+			silverCapPerc = silverCapPerc;
+		}
+		if(goldCapPerc >= 100)
+		{
+			goldCapPerc = 100;
+			gold.SetActive(true);
+			silver.SetActive(false);
+			Neutral.SetActive(false);
+		}
+		if(silverCapPerc >= 100)
+		{
+			silverCapPerc = 100;
+			gold.SetActive(false);
+			silver.SetActive(true);
+			Neutral.SetActive(false);
+		}
+		if(goldCapPerc <= 50 && silverCapPerc <= 51)
+		{
+			gold.SetActive(false);
+			silver.SetActive(false);
+			Neutral.SetActive(true);
+		}
+		if(goldCapPerc <= 0)
+		{
+			goldCapPerc = 0;
+		}
+		if(silverCapPerc <= 0)
+		{
+			silverCapPerc = 0;
+		}
+	}
+//	void OnGUI()
+//	{
+//		GUI.Box(Rect(10, 10, 300, 25), "Gold Cap" + " " + goldCapPerc.ToString(" ") + " " + "Silver Cap" + " " + silverCapPerc.ToString(" "));
+//	}
+}
+
