921d64a24b0fd339a08d2cb85f74e9175926e916
RichieDevEire <c15707085@mydit.ie>
2017-03-07 02:41:23 +0000

    Surprise for tomorrows meet up :D

--- Assets/TeamScoreGold.cs
+++ Assets/TeamScoreGold.cs
@@ -11,6 +11,10 @@ public class TeamScoreGold : MonoBehaviour
 
 	public GameObject Orb;
 
+	public GameObject []Goldpaper;
+	public GameObject []Silverpaper;
+
+
 	private int goldCount;
 	private int silverCount;
 
@@ -48,16 +52,50 @@ public class TeamScoreGold : MonoBehaviour
 	void SetGoldScoreText()
 	{
 		goldScoreText.text = "Gold: " + goldCount.ToString ();
+		if(goldCount == 1)
+		{
+			Goldpaper[0].SetActive(true);
+		}
+		if(goldCount == 2)
+		{
+			Goldpaper[1].SetActive(true);
+		}
+		if(goldCount == 3)
+		{
+			Goldpaper[2].SetActive(true);
+		}
+		if(goldCount == 4)
+		{
+			Goldpaper[3].SetActive(true);
+		}
 		if(goldCount >= 5)
 		{
+			Goldpaper[4].SetActive(true);
 			winText.text = "Gold Team Win";
 		}
 	}
 	void SetSilverScoreText()
 	{
 		silverScoreText.text = "Silver: " + silverCount.ToString ();
+		if(silverCount == 1)
+		{
+			Silverpaper[0].SetActive(true);
+		}
+		if(silverCount == 2)
+		{
+			Silverpaper[1].SetActive(true);
+		}
+		if(silverCount == 3)
+		{
+			Silverpaper[2].SetActive(true);
+		}
+		if(silverCount == 4)
+		{
+			Silverpaper[3].SetActive(true);
+		}
 		if(silverCount >= 5)
 		{
+			Silverpaper[4].SetActive(true);
 			winText.text = "Silver Team Win";
 		}
 	}
